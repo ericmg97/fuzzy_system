43f4c48088d4e1f16996310f0c93484da01b5f7d
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-11-17 18:21:14 -0500

    Add fuzzy_rule
    *min_operator for Mamdani method on fuzzy_set

---
+++ src/fuzzy_rule.py
@@ -0,0 +1,22 @@
+class FuzzyRule():
+
+	def __init__(self):
+		self._antecedent = []
+		self._consequent = {}
+
+	def add_antecedent_clause(self, f_set):
+		self._antecedent.append(f_set)
+
+	def add_consequent_clause(self, var, f_set):
+		self._consequent[var] = f_set
+
+	def evaluate(self):			
+		rule_min = 1
+		for ante_clause in self._antecedent:
+			rule_min = min(ante_clause.last_degree_value , rule_min)
+
+		for consequent_clause in self._consequent.items():
+			c_var = consequent_clause[0]
+			c_set = consequent_clause[1]
+			c_var.add_rule_contribution(c_set.min_operator(rule_min))
+			
