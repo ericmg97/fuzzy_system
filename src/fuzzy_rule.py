b2d62e855a48e4e9ce8bd3cc1c8349a31bee08e5
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-11-17 18:28:18 -0500

    Add inference method possibilities
    *Mamdani already implemented

--- src/fuzzy_rule.py
+++ src/fuzzy_rule.py
@@ -10,13 +10,15 @@ class FuzzyRule():
 	def add_consequent_clause(self, var, f_set):
 		self._consequent[var] = f_set
 
-	def evaluate(self):			
+	def evaluate(self, method):
 		rule_min = 1
 		for ante_clause in self._antecedent:
-			rule_min = min(ante_clause.last_degree_value , rule_min)
+			rule_min = min(ante_clause.last_degree_value, rule_min)
 
 		for consequent_clause in self._consequent.items():
 			c_var = consequent_clause[0]
 			c_set = consequent_clause[1]
-			c_var.add_rule_contribution(c_set.min_operator(rule_min))
+			
+			if method == "Mamdani":
+			    c_var.add_rule_contribution(c_set.min_operator(rule_min))
 			
