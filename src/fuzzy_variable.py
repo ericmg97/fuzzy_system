8a60f4678433af46e04426b8d97510e349b1d282
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-11-17 21:40:30 -0500

    Add system_logic
    *Separate Variables on input - output

--- src/fuzzy_variable.py
+++ src/fuzzy_variable.py
@@ -19,3 +19,23 @@ class Variable():
             self._min_val, self._max_val, low, mid1, mid2, high)
         self.sets[name] = new_set
         return new_set
+
+class InputVariable(Variable):
+
+	def __init__(self, name, min_val, max_val):
+		super().__init__(name, min_val, max_val)
+
+	def fuzzify(self, value):
+		for _, f_set in self.sets.items():
+			f_set.last_degree_value = f_set[value]
+
+class OutputVariable(Variable):
+    def __init__(self, name, min_val, max_val):
+        super().__init__(name, min_val, max_val)
+        self._output_distribution = FuzzySet(min_val, max_val)
+    
+    def add_rule_contribution(self, rule_consequence):
+        self._output_distribution = self._output_distribution.union(rule_consequence)
+    
+    def defuzzify(self, def_method):
+        return self._output_distribution.defuzzify(def_method)
